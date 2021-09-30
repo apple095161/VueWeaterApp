4f2dfb442e76a8c91beaa55e3546036984a1b89e
unknown <apple095161@gmail.com>
2021-09-30 22:51:59 +0800

    upload

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">Weater</div>
+</template>
+
+<script>
+import { ref, reactive, onMounted } from "vue";
+export default {
+  name: "Home",
+  setup() {
+    const array = ref([0, 1, 2, 3, 4]);
+    const array2 = reactive({});
+    onMounted(() => {
+      console.log("123");
+    });
+    // onMounted(() => {
+    //   array.value.forEach((item) => {
+    //     console.log(item);
+    //   });
+    // });
+    return {
+      array,
+      array2,
+    };
+  },
+};
+</script>
